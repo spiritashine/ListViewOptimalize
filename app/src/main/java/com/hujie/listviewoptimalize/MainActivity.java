2e1047460a15cdb614a8197e87207183dc9894bc
hujie <380211629@qq.com>
2017-01-09 10:55:13 +0800

    inflater a View from XML and add it into ListView

--- app/src/main/java/com/hujie/listviewoptimalize/MainActivity.java
+++ app/src/main/java/com/hujie/listviewoptimalize/MainActivity.java
@@ -4,8 +4,10 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -17,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ListView listView= (ListView) findViewById(R.id.lv);
+        LinearLayout linearLayout= (LinearLayout) LayoutInflater.from(this).inflate(R.layout.banner,null);
         ImageView imageView=new ImageView(this);
         Toolbar toolbar=new Toolbar(this);
         toolbar.setLogo(R.mipmap.ic_launcher);
@@ -25,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         imageView.setImageResource(R.mipmap.ic_launcher);
         listView.addHeaderView(imageView);
         listView.addHeaderView(toolbar);
+        listView.addFooterView(linearLayout);
         listView.addFooterView(imageView);
         ArrayList<String> data=new ArrayList<>();
         for (int i=0;i<30;i++){
